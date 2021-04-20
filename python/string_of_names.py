6a1ed3dc5f0db4e29a34bc78efebe0de8f5f01f0
Gerald Southwood <72921662+VertX-G@users.noreply.github.com>
2021-04-20 15:25:47 +0200

    Update string_of_names.py

--- python/string_of_names.py
+++ python/string_of_names.py
@@ -1,3 +1,5 @@
+# Format a string of names like 'Bart, Lisa & Maggie'.
+
 # Given an array containing hashes of names
 # Return a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
 
